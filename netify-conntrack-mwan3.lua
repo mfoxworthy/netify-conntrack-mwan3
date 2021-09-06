f6378be3e45be9455c4ff2a15a2414583aca6938
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-05 19:47:41 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -101,14 +101,14 @@ function pipeconntrack (marks)
     
     -- We need to know if the NEW connection is TCP or UDP.
     -- conntrack formats these lines differently
-    
+    print(marks[f_mark])
     if (status == "NEW" and conn_arr [2] == "tcp")
        then
         dst_IP = string.gsub(conn_arr [7], "dst%=", "")
         f_mark = string.gsub(conn_arr [15], "mark%=", "")
         print('main function fmark ' .. f_mark)
         test_reset = testconntrack(f_mark, dst_IP, marks)
-        print(marks[f_mark])
+        
         print(test_reset)
         -- print("tcp flow ", dst_IP)
         --reset(dst_IP)
