61c6f890ea3bd0a595c8c60fff81d3475fcee106
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-05 18:13:43 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -58,6 +58,7 @@ function fetchmarks(policy, ipsets)
   for i, v in ipairs(policy) do
     local markcmd = assert(io.popen('iptables -L ' .. v .. ' -t mangle | grep MARK | awk \'{print $16}\' | cut -c -5'), 'r')
       for m in markcmd:lines() do
+        print(ipsets[i])
         key = tostring(ipsets[i])
         print(key)
         marks.key = m
