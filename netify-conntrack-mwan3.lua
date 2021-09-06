de26d8d9c70a1af4f487f68782381476fd21017b
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-05 22:00:08 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -120,7 +120,10 @@ function pipeconntrack (marks)
         local dst_IP = string.gsub(conn_arr [7], "dst%=", "")
         local f_mark = string.gsub(conn_arr [15], "mark%=", "")
         local test_reset = testconntrack(f_mark, dst_IP, marks)
-        
+        if (test_reset == 1)
+          then
+            reset(dst_IP)
+        end
         print(test_reset)
         -- print("tcp flow ", dst_IP)
         --reset(dst_IP)
