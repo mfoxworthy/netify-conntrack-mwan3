dfbea007c099b753cc58789788d00b088d6b2b32
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-06 12:00:42 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -34,8 +34,8 @@ function flow_reset (dst_IP, set, del_set)
   local reset = "conntrack -D -d " .. dst_IP
   os.execute('ipset del ' .. del_set .. ' ' .. dst_IP)
   os.execute('ipset add ' .. set .. ' ' .. dst_IP)
-  sleep(1)
-  os.execute(reset)
+  --sleep(1)
+  -- os.execute(reset)
   os.execute('logger -p notice -t conntrack_fix \'Made ipset correction for \'' .. dst_IP)
 end
 
