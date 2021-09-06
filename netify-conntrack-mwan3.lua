d74a1c9db5a7a92f3464e41516767562449d1987
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-06 13:08:47 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -101,7 +101,7 @@ function fixconntrack (f_mark, dst_IP, g_marks)
             mark_check = mark_check + 1
         end
         set_count = set_count + 1
-        local conncheckcmd = 'ipset list ' .. v .. ' | grep timeout | grep -v Header | awk \'{print $1}\''
+        local conncheckcmd = 'ipset list ' .. v .. ' | tail -n +<N+1>''
         if (logging == 1)
           then
             os.execute('logger -p notice -t conntrack_fix \'Checking set \'' .. v)
