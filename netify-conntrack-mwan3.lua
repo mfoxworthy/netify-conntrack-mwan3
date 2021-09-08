10462f7ea2b68878b71888e89dcfc8fba81cd46f
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-08 12:13:33 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -49,7 +49,7 @@ function nolog()
 
 
 function logger (level, message)
-  local loglvl = loglvl_arr[level]
+  loglvl = loglvl_arr[level]
   if (loglvl ~= nil and logging_level ~= 0) then
     loglvl(message)
   else
