73571a3cc72d31e854d08ad8ab6045fbf1b4b775
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-09 23:54:39 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -21,35 +21,6 @@ function sleep (n)
     end
 end
 
-
-
--- String splitter function. Just handy for string manipulation.
-
-function string:split(sSeparator, nMax, bRegexp)
-   assert(sSeparator ~= '')
-   assert(nMax == nil or nMax >= 1)
-
-   local aRecord = {}
-
-   if self:len() > 0 then
-      local bPlain = not bRegexp
-      nMax = nMax or -1
-
-      local nField, nStart = 1, 1
-      local nFirst,nLast = self:find(sSeparator, nStart, bPlain)
-      while nFirst and nMax ~= 0 do
-         aRecord[nField] = self:sub(nStart, nFirst-1)
-         nField = nField+1
-         nStart = nLast+1
-         nFirst,nLast = self:find(sSeparator, nStart, bPlain)
-         nMax = nMax-1
-      end
-      aRecord[nField] = self:sub(nStart)
-   end
-
-   return aRecord
-end
-
 --Logging logic. Probably find a logging lib somewhere and replace.
 
 function loglvl1(message)
