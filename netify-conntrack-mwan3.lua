6888580acf048dae331d461cdabca313ce8a16bb
Mike Foxworthy <mike.foxworthy@viasat.com>
2021-09-11 10:08:29 -0700

    Update netify-conntrack-mwan3.lua

--- netify-conntrack-mwan3.lua
+++ netify-conntrack-mwan3.lua
@@ -212,7 +212,7 @@ function nf_conntrack (nf_mark)
             dst_IP = string.gsub(conn_arr [6], "dst%=", "")
           end
         else
-          logger(1, '\'Connection is not eligable for reset...\'')
+          logger(3, '\'Connection is not eligable for reset...\'')
         end
     end
   end
